4e6787d9a705b0497035c46e6a21c6c50f3040b0
youngmin <min050410@gmail.com>
2022-01-31 23:55:47 +0900

    add: quick sort

---
+++ Sort/quick.py
@@ -0,0 +1,17 @@
+array = [5, 7, 9,0, 3, 1, 0]
+
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+    
+    pivot = array[0]
+    tail = array[1:]
+
+    left_side = [x for x in tail if x <= pivot] # 분할한 왼쪽 부분
+    right_side = [x for x in tail if x > pivot] # 분할한 오른쪽 부분
+
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬을 수행하고, 전체 리스트를 반환
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
+
