fb8c0199847b02db4c4123273724f66c3cc7db0f
min050410 <min050410@gmail.com>
2022-03-20 16:20:42 +0900

    fix: commit bug

---
+++ math/cpp/1316.cpp
@@ -0,0 +1,36 @@
+// 그룹 단어 체커
+
+#include<iostream>
+
+using namespace std;
+
+int n, cnt;
+
+bool isGroup(const char *a){
+    int arr[200]={};
+    for (int i=0; a[i]!='\0'; i++){
+        if (arr[a[i]]>=1){
+            if(a[i] != a[i-1]){
+                return false;
+            }
+        }
+        else{
+            arr[a[i]]++;
+        }
+    }
+    return true;
+}
+
+int main(){
+    cin >> n;
+    for (int i=0; i<n; i++){
+        char input[101]={};
+        cin >> input;
+        if(isGroup(input)) {
+            cnt++;
+        }
+    }
+    cout << cnt;
+}
+
+
