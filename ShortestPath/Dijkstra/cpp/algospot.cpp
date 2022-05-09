284762954e6ea941c6b0af1e0f339c054286e056
youngmin <min050410@gmail.com>
2022-05-10 08:10:25 +0900

    add: 주석

--- ShortestPath/Dijkstra/cpp/algospot.cpp
+++ ShortestPath/Dijkstra/cpp/algospot.cpp
@@ -1,5 +1,4 @@
 // 알고스팟 
-
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -14,23 +13,31 @@ int dis[100][100];
 int mx[4] = {1, 0, -1, 0};
 int my[4] = {0, 1, 0, -1};
 
-// weight, x_position, y_position
+// weight, y_position, x_position
 priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>> ,greater<>> pq;
 
 void dijkstra(int sx, int sy) {
+    // 초기 위치 비용 설정
     dis[sy][sx] = v[sy][sx];
+    // pq에 초깃값 푸시
     pq.push({v[sy][sx], sy, sx});
     while (!pq.empty()) {
+        // 가중치
         int w = get<0>(pq.top());
+        // y_position, x_position
         pair<int, int> temp = make_pair(get<1>(pq.top()), get<2>(pq.top()));
+        // 변수에 담았으므로 pop()
         pq.pop();
         for (int i=0; i<4; i++){
             // 이동한 뒤 좌표
             int nx = temp.second + mx[i];
             int ny = temp.first + my[i];
+            // 범위 설정
             if (ny >= 0 && ny < m && nx >= 0 && nx < n){
+                // dis 배열 초기화 min(dis[][], w + v[ny][nx]) 
                 if (dis[ny][nx] > w + v[ny][nx]){
                     dis[ny][nx] = w + v[ny][nx];
+                    // 결과 푸시 (가중치를 제일 앞에)
                     pq.push({dis[ny][nx], ny, nx});
                 }
             }
