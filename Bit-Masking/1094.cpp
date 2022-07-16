b3d38cf3b1a39aa93b7b1735a58839f7cfec9549
min050410 <min050410@gmail.com>
2022-07-16 22:55:53 +0900

    add: 막대기

---
+++ Bit-Masking/1094.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int len;
+
+int bitCount(int x) {
+    if (x==0) return 0;
+    return x % 2 + bitCount(x / 2);
+}
+
+
+int main() {
+    cin >> len;
+    cout << bitCount(len);
+}
