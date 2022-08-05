31f74cadfa472a672c2059f408daf8b6082aeb6e
min050410 <min050410@gmail.com>
2022-08-05 23:49:31 +0900

    add: 동전 0

---
+++ GREEDY/11047.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int arr[12];
+int main(){
+	int a,b,c=0;
+	cin >> a >> b;
+	for (int i = 1;i <= a; i++){
+		cin >> arr[i];
+	}
+	int i = a;
+	while (b){
+		while (b - arr[i] >= 0){
+			b -=arr[i];
+			c++;
+		}
+		i--;
+	}	
+	cout << c;
+}
