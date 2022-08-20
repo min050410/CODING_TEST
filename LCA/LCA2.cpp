5438eeef4a1ab6e5eae6683c26f9eb136133f5cf
min050410 <min050410@gmail.com>
2022-08-20 23:19:36 +0900

    add: LCA 2

---
+++ LCA/LCA2.cpp
@@ -0,0 +1,88 @@
+// LCA
+#include <iostream>
+#include <vector>
+#define LOG 21 // 2^20 = 1000000
+
+using namespace std;
+
+int n, m;
+int parent[10000][10000];
+int d[100000];
+bool v[100000]; // 계산 여부
+vector<int> graph[100000];
+int a, b;
+
+void SwapValue(int &a, int &b) {
+    int t = a;
+    a = b;
+    b = t;
+}
+
+// depth check
+void dfs(int x, int depth) {
+    v[x] = true;
+    d[x] = depth;
+    for (int g: graph[x]) {
+        if (v[g]) continue;
+        parent[g][0] = x;
+        dfs(g, depth+1);
+    }
+}
+
+void set_parent() {
+    dfs(1, 0);
+    for (int i=1; i<LOG; i++) {
+        for (int j=1; j<n+1; j++) {
+            parent[i][j] = parent[parent[j][i-1]][i-1];
+        }
+    }
+}
+
+int lca(int a, int b) {
+
+    if (d[a] > d[b]) {
+        SwapValue(a, b);
+    }
+
+    for (int i=LOG-1; i>=0; i--) {
+        if (d[b] - d[a] >= (1 << i)) {
+            b = parent[b][i];
+        }
+    }
+
+    if (a == b) {
+        return a;
+    }
+
+    for (int i=LOG-1; i>-1; i--) {
+        if (parent[a][i] != parent[a][i]) {
+            a = parent[a][i];
+            b = parent[b][i];
+        }
+    }
+
+    return parent[a][0];
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n;
+
+    for (int i=0; i<n-1; i++) {
+        cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    set_parent();
+
+    cin >> m;
+    for (int i=0; i<m; i++) {
+        cin >> a >> b;
+        cout << lca(a, b) << '\n';
+    }
+}
