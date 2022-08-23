ca76f7b292dfc8b22808400e221537120e2b6629
youngmin <min050410@gmail.com>
2022-08-23 14:12:17 +0900

    add: 참외밭 AC 수정

--- Geometry/polygon/2477.cpp
+++ Geometry/polygon/2477.cpp
@@ -1,38 +1,38 @@
 // 참외밭
 
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#define ll long long
+#define inf 0x7fffffff
+#define N 6
+using namespace std;
 
-int N;
-int arr[6 + 2][2 + 2];  // 주어지는 방향, 길이쌍
-int cnt[4]; // 방향 별 등장 횟수 저장
+int k;
+int a, b;
+// 방향, 길이
+pair<int, int> arr[10000];
+int s;
+int small_s;
 
-int main(void) {
-    
-    int s = 1; // 작은 사각형의 넓이
-    int b = 1; // 큰 사각형의 넓이
-
-    // 입력 받기
-    scanf("%d", &N);
-    for (int i = 0; i < 6; i++) {
-        scanf("%d %d", &arr[i][0], &arr[i][1]);
-        cnt[arr[i][0]]++;
-    }
-
-    for (int i = 0; i < 6; i++) {
-
-        if (cnt[arr[i][0]] == 1) {
-            // 한 번씩 등장하는 방향이다
-            b *= arr[i][1]; // 큰 사각형의 너비/높이이다
-            continue;
+int main() {
+    cin >> k;
+    for (int i=0; i<N; i++){
+        cin >> a >> b;
+        arr[i].first = a;
+        arr[i].second = b;
+        if (i == 0 || i == 1 || i == 2) {
+            arr[i+N].first = a;
+            arr[i+N].second = b;
         }
-
-        int n = (i + 1) % 6;
-        int nn = (i + 2) % 6;
-        // 나(I)랑 다다음거(nn)랑 같으면 그 사이에 있는 것(n)이 작은 사각형의 너비/높이이다.
-        if (arr[i][0] == arr[nn][0]) s *= arr[n][1];
     }
 
-    printf("%d", (b - s) * N);
-
-    return 0;
+    for (int i=0; i<N; i++){
+        if (arr[i].first == arr[i+2].first && arr[i+1].first == arr[i+3].first) {
+            small_s = arr[i+1].second * arr[i+2].second;
+            s = (arr[i].second + arr[i+2].second) * (arr[i+1].second + arr[i+3].second);
+        }
+    }
+//    cout << s << ' ' << small_s;
+    cout << k * (s - small_s);
 }
+
